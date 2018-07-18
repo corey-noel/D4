bee96f97cca84e62e4711191dec1ccaaa57d650d
Corey Noel <corey.g.noel@gmail.com>
2018-07-18 14:03:56 -0400

    removed some test lines in verifier.rb

--- verifier.rb
+++ verifier.rb
@@ -6,6 +6,3 @@ end
 def hash_char_val(x)
   (x ** 2000) * ((x + 2) ** 21) - ((x + 5) ** 3)
 end
-
-str_in = "8|e01d|Sam>John(3):Joe>Sam(4):SYSTEM>Rana(100)|1518839370.605237540"
-puts calculate_hash str_in
