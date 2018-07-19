a1f6d1042fff2564e8a25b09321d4daa900c3ee7
Corey Noel <corey.g.noel@gmail.com>
2018-07-19 19:38:41 -0400

    all parsing except for transaction lists

--- verifier.rb
+++ verifier.rb
@@ -2,14 +2,13 @@ require_relative 'char_table.rb'
 
 # opens and reads a file, parsing each line to a block
 def read_block_chain filename
-  line_num = 1
   block_chain = []
-  File.open(filename).each do |block|
+  File.open(filename).each_with_index do |block, line|
     begin
       block_chain << parse_block split_block block
-      line_num += 1
     rescue ArgumentError => err
-      puts "Error on line #{line_num}: #{err.message}"
+      puts "Error on line #{line + 1}: #{err.message}"
+      quit -1
     end
   end
   block_chain
@@ -17,10 +16,10 @@ end
 
 # takes in a string representation of a block
 # returns an array of strings of block parts
-# raises an error on the wrong number of elements
+# raises an ArgumentError on the wrong number of elements
 def split_block str_in
   str_parts = str_in.split("|")
-  raise ArgumentError("Requires 5 elements (received #{str_parts.length})") unless str_parts.length == 5
+  raise ArgumentError("Block requires 5 elements (got #{str_parts.length})") unless str_parts.length == 5
   str_parts
 end
 
@@ -29,15 +28,15 @@ end
 def parse_block str_parts
   block_id = parse_id(str_parts[0])
   prev_hash = parse_hash(str_parts[1])
-  transaction_list = nil #TODO
-  time_stamp = nil #TODO
+  transaction_list = parse_transaction_list(str_parts[2])
+  seconds, nanonseconds = parse_time_stamp(str_parts[3])
   block_hash = parse_hash(str_parts[4])
   # Block.new TODO
 end
 
 # takes in a string representing the ID
 # returns an integer ID
-# raises an error on failure to parse
+# raises an ArgumentError on failure to parse
 def parse_id id_str
   begin
     Integer(id_str)
@@ -48,12 +47,30 @@ end
 
 # takes in a string representation of a hash
 # returns hex integer of hash
-# raises an error on failure to parse
+# raises an ArgumentError on failure to parse
 def parse_hash hash_in
   begin
     Integer(hash_in, 16)
   rescue ArgumentError
-    raise ArgumentError("Failed to parse hash")
+    raise ArgumentError("Could not parse hash")
+  end
+end
+
+# TODO
+def parse_transaction_list transaction_in
+  nil
+end
+
+# takes in a string representation of a time_stamp
+# returns two integers: seconds and nanoseconds
+# raises an ArgumentError on failure to parse
+def parse_time_stamp ts_in
+  ts_parts = ts_in.split(".")
+  raise ArgumentError("Timestamp requires 2 parts (got #{ts_parts.length}") unless ts_parts.length == 2
+  begin
+    Integer(ts_parts[0]), Integer(ts_parts[1])
+  rescue ArgumentError
+    raise ArgumentError("Could not parse timestamp")
   end
 end
 
@@ -73,3 +90,5 @@ end
 def hash_char_val(char)
   (char**2000) * ((char + 2)**21) - ((char + 5)**3)
 end
+
+read_block_chain(ARGV[0])
