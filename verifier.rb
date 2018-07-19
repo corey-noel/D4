ba89fe89b2c6625a63e3c59965e7fa08b000d7c5
Corey Noel <corey.g.noel@gmail.com>
2018-07-19 18:58:25 -0400

    added more parsing

--- verifier.rb
+++ verifier.rb
@@ -1,5 +1,49 @@
 require_relative 'char_table.rb'
 
+def read_block_chain filename
+  line_num = 1
+  block_chain = []
+  File.open(filename).each do |block|
+    begin
+      block_chain << parse_block split_block block
+      line_num += 1
+    rescue ArgumentError => err
+      puts "Error on line #{line_num}: #{err.message}"
+    end
+  end
+  block_chain
+end
+
+def split_block str_in
+  str_parts = str_in.split("|")
+  raise ArgumentError("Requires 5 elements (received #{str_parts.length})") unless str_parts.length == 5
+  str_parts
+end
+
+def parse_block str_parts
+  block_id = parse_id(str_parts[0])
+  prev_hash = parse_hash(str_parts[1])
+  transaction_list = nil #TODO
+  time_stamp = nil #TODO
+  block_hash = parse_hash(str_parts[4])
+end
+
+def parse_id id_str
+  begin
+    Integer(id_str)
+  rescue ArgumentError
+    raise ArgumentError("Could not parse ID to an integer")
+  end
+end
+
+def parse_hash hash_in
+  begin
+    Integer(hash_in, 16)
+  rescue ArgumentError
+    raise ArgumentError("Failed to parse hash")
+  end
+end
+
 # HASHING FUNCTIONS
 # Performs a hash on the entire string
 def calculate_hash(str_in, hash_method=:hash_lookup)
@@ -15,35 +59,4 @@ end
 # gets the hash value of a single char via calculation
 def hash_char_val(char)
   (char**2000) * ((char + 2)**21) - ((char + 5)**3)
-end
-
-def verify_hash(str_parts)
-  calculate_hash(str_parts[0..3].join("|"))
-end
-
-
-# PARSE FUNCTIONS
-# parses a single block
-def parse_block str_in
-  str_parts = split_block(str_in)
-  block_id = Integer(str_parts[0])
-  last_hash = parse_hash(str_parts[1])
-
-  given_hash = parse_hash(str_parts[4])
-  verify_hash(str_parts)
-end
-
-# parses a hash into an integer
-def parse_hash hash_in
-  begin
-    Integer(hash_in, 16)
-  rescue ArgumentError
-    raise ArgumentError("Failed to parse hash")
-  end
-end
-
-def split_block
-  str_parts = str_in.split("|")
-  raise ArgumentError("Requires 5 elements (received #{str_parts.length})") unless str_parts.length == 5
-  str_parts
 end
