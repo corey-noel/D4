74d2cc8f4c20d7cb7849ed99940bc1ca56e5aa30
Corey Noel <corey.g.noel@gmail.com>
2018-07-24 15:22:29 -0400

    split verifier into hashing and parsing files, rubocop verification

--- hashing.rb
+++ hashing.rb
@@ -1,9 +1,8 @@
 require_relative 'char_table.rb'
 
-
 # HASHING FUNCTIONS
 # Performs a hash on the entire string
-def calculate_hash(str_in, hash_method=:hash_lookup)
+def calculate_hash(str_in, hash_method = :hash_lookup)
   sum = str_in.unpack('U*').map(&method(hash_method)).reduce(:+)
   (sum % 65536).to_s(16)
 end
--- parsing.rb
+++ parsing.rb
@@ -1,5 +1,5 @@
 # opens and reads a file, parsing each line to a block
-def read_block_chain filename
+def read_block_chain(filename)
   block_chain = []
   File.open(filename).each_with_index do |block, line|
     begin
@@ -15,15 +15,15 @@ end
 # takes in a string representation of a block
 # returns an array of strings of block parts
 # raises an ArgumentError on the wrong number of elements
-def split_block str_in
-  str_parts = str_in.split("|")
+def split_block(str_in)
+  str_parts = str_in.split('|')
   raise ArgumentError("Block requires 5 elements (got #{str_parts.length})") unless str_parts.length == 5
   str_parts
 end
 
 # takes in an array of block parts
 # returns a block object
-def parse_block str_parts
+def parse_block(str_parts)
   block_id = parse_id(str_parts[0])
   prev_hash = parse_hash(str_parts[1])
   transaction_list = parse_transaction_list(str_parts[2])
@@ -31,7 +31,7 @@ def parse_block str_parts
   block_hash = parse_hash(str_parts[4])
   # Block.new TODO
 
-  puts "#" * 20
+  puts '#' * 20
   puts block_id
   puts prev_hash
   puts transaction_list
@@ -43,39 +43,35 @@ end
 # takes in a string representing the ID
 # returns an integer ID
 # raises an ArgumentError on failure to parse
-def parse_id id_str
-  begin
-    Integer(id_str)
-  rescue ArgumentError
-    raise ArgumentError("Could not parse ID to an integer")
-  end
+def parse_id(id_str)
+  Integer(id_str)
+rescue ArgumentError
+  raise ArgumentError('Could not parse ID to an integer')
 end
 
 # takes in a string representation of a hash
 # returns hex integer of hash
 # raises an ArgumentError on failure to parse
-def parse_hash hash_in
-  begin
-    Integer(hash_in, 16)
-  rescue ArgumentError
-    raise ArgumentError("Could not parse hash")
-  end
+def parse_hash(hash_in)
+  Integer(hash_in, 16)
+rescue ArgumentError
+  raise ArgumentError('Could not parse hash')
 end
 
 # TODO
-def parse_transaction_list transaction_in
+def parse_transaction_list(transaction_in)
   nil
 end
 
 # takes in a string representation of a time_stamp
 # returns two integers: seconds and nanoseconds
 # raises an ArgumentError on failure to parse
-def parse_time_stamp ts_in
-  ts_parts = ts_in.split(".")
+def parse_time_stamp(ts_in)
+  ts_parts = ts_in.split('.')
   raise ArgumentError("Timestamp requires 2 parts (got #{ts_parts.length}") unless ts_parts.length == 2
   begin
     return Integer(ts_parts[0]), Integer(ts_parts[1])
   rescue ArgumentError
-    raise ArgumentError("Could not parse timestamp")
+    raise ArgumentError('Could not parse timestamp')
   end
 end
