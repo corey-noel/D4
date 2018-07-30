317944b50c7e85e0ee5a07e213a09ad3b7394493
Corey Noel <corey.g.noel@gmail.com>
2018-07-29 22:31:37 -0400

    fleshed out transaction.rb

--- transaction.rb
+++ transaction.rb
@@ -1,10 +1,19 @@
 class Transaction
-
   attr_reader :from_name, :to_name, :amt
 
   def initialize(from_name, to_name, amt)
     @from_name = from_name
     @to_name = to_name
     @amt = amt
+
+    @system = from_name == "SYSTEM"
+  end
+
+  def system?
+    @system
+  end
+
+  def to_s
+    "#{@from_name}>#{@to_name}(#{@amt})"
   end
 end
