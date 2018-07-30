a4f0097e23959960b6ebb10f7daef73979db726f
Corey Noel <corey.g.noel@gmail.com>
2018-07-30 06:59:41 -0400

    most testing for parsing.rb

---
+++ test/parsing_test.rb
@@ -0,0 +1,106 @@
+require_relative '../parsing.rb'
+require 'minitest/autorun'
+
+class ParsingTest < MiniTest::Test
+
+  # tests that the length of the array returned by
+  # split_block is correct
+  def test_split_block_length
+    in_str = "0|abcd|you>me(100)|100.200|sdfg"
+    assert_equal 5, split_block(in_str).length
+  end
+
+  # tests the contents of the array returned by split_block
+  def test_split_block_length
+    in_str = "0|abcd|you>me(100)|100.200|sdfg"
+    out_arr = ["0", "abcd", "you>me(100)", "100.200", "sdfg"]
+    assert_equal out_arr, split_block(in_str)
+  end
+
+  # tests that split_block raises an error when the input is too long
+  def test_split_block_too_long
+    in_str = "0|abcd|you>me(100)|100.200|sdfg|???"
+    assert_raises(ArgumentError) { split_block(in_str) }
+  end
+
+  # tests that split_block raises an error when the input is too short
+  def test_split_block_too_short
+    in_str = "0|you>me(100)|100.20|sdfg"
+    assert_raises(ArgumentError) { split_block(in_str) }
+  end
+
+  # tests that parse_id returns a successful parse correctly
+  def test_parse_id_valid
+    assert_equal 19, parse_id("19")
+  end
+
+  # tests that parse_id raises an error when it can't parse
+  def test_parse_id_invalid
+    assert_raises(ArgumentError) { parse_id("bad") }
+  end
+
+  # tests that parse_hash returns a valid hash correctly
+  def test_parse_hash_valid
+    assert_equal 100000, parse_hash("186A0")
+  end
+
+  # tests that parse_hash will raise an error when it can't parse
+  def test_parse_hash_invalid
+    assert_raises(ArgumentError) { parse_hash("not_hex") }
+  end
+
+  def test_parse_transaction_list_valid
+    in_str = "you>me(100):me>you(2):bob>george(19)"
+    assert_equal 3, parse_transaction_list(in_str).length
+  end
+
+  def test_parse_transaction_list_invalid_syntax
+    in_str = "you>me(100):me>you(2)::bob>george(19)"
+    assert_raises(ArgumentError) { parse_transaction_list(in_str) }
+  end
+
+  def test_parse_transaction_src
+    in_str = "you>me(200)"
+    assert_equal "you", parse_transaction(in_str).src
+  end
+
+  def test_parse_transaction_dest
+    in_str = "you>me(200)"
+    assert_equal "me", parse_transaction(in_str).dest
+  end
+
+  def test_parse_transaction_amt
+    in_str = "you>me(200)"
+    assert_equal 200, parse_transaction(in_str).amt
+  end
+
+  def test_parse_transaction_invalid_syntax
+    in_str = "you?me(4000)"
+    assert_raises(ArgumentError) { parse_transaction(in_str) }
+  end
+
+  def test_parse_timestamp_seconds
+    in_str = "12345.6789"
+    seconds, _ = parse_time_stamp(in_str)
+    assert_equal 12345, seconds
+  end
+
+  def test_parse_timestamp_nanoseconds
+    in_str = "12345.6789"
+    _, nanoseconds = parse_time_stamp(in_str)
+    assert_equal 6789, nanoseconds
+  end
+
+  def test_parse_timestamp_too_long
+    in_str = "123.456.678"
+    assert_raises(ArgumentError) { parse_time_stamp(in_str) }
+  end
+
+  def test_parse_block
+
+  end
+
+  def test_parse_block_invalid
+
+  end
+end
