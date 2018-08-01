1ce1d51b99c952809634909bd1807686dfaaf495
Corey Noel <corey.g.noel@gmail.com>
2018-08-01 00:01:24 -0400

    hashing tests

---
+++ test/hashing_test.rb
@@ -0,0 +1,25 @@
+require_relative '../hashing.rb'
+require 'minitest/autorun'
+
+class HashingTest < MiniTest::Test
+
+  def test_char_lookup
+    assert_equal 5667, hash_lookup(9)
+  end
+
+  def test_char_calculate
+    assert_equal 5667, hash_char_val(9) % 65536
+  end
+
+  def test_calculate_hash_default
+    assert_equal 17017, calculate_hash("hello")
+  end
+
+  def test_calculate_hash_lookup
+    assert_equal 17017, calculate_hash("hello", :hash_lookup)
+  end
+
+  def test_calculate_hash_calculate
+    assert_equal 17017, calculate_hash("hello", :hash_char_val)
+  end
+end
--- test/main_test.rb
+++ test/main_test.rb
@@ -4,3 +4,4 @@ SimpleCov.start
 require 'minitest/autorun'
 require_relative 'parsing_test.rb'
 require_relative 'transaction_test.rb'
+require_relative 'hashing_test.rb'
